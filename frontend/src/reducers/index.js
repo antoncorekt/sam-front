b57acf928dbc93e6701058d6cf67aab87f5480c8
Tomasz Kabała <Tomasz.Kabala@external.t-mobile.pl>
2019-10-08 18:52:45 +0200

    Segments

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,19 +1,26 @@
 import { combineReducers } from "redux";
 import { createReducer } from "../api/common-reducers";
-import { tabsState } from "./tabReducer";
-import { PostUserInfoHandler, PostUserLoginHandler, PostUserLogoffHandler, UnauthorizedHandler } from "./auth/auth-reducer";
-import { RequestPanelHandler, RequestPanelType } from "./request-panel-reducer";
+
 import { AuthType } from "./auth/auth-store-type";
+import { SapAccountStoreType } from "./sap-account/sap-account-store-type";
+import { SegmentsType } from "./segments/segments-store-type";
+import { AccountMappingType } from "./bscs-account/bscs-account-store-type";
+
+import { PostUserInfoHandler, PostUserLoginHandler, PostUserLogoffHandler, UnauthorizedHandler } from "./auth/auth-reducer";
+import { GetSystemVersionHandler } from "../api/api-handlers";
+import { RequestPanelHandler, RequestPanelType } from "./request-panel-reducer";
 import {
     DeleteDictionaryAccountSapHandler,
     GetDictionaryAccountSapHandler,
     PostDictionaryAccountSapHandler
 } from "./sap-account/sap-account-reducer";
-import { SapAccountStoreType } from "./sap-account/sap-account-store-type";
-import { GetDictionarySegmentHandler, PostDictionarySegmentHandler, SegmentPropertiesInReduxHandler } from "./segments/segments-reducer";
-import { GetSystemVersionHandler } from "../api/api-handlers";
+import {
+    GetDictionarySegmentHandler,
+    PostDictionarySegmentHandler,
+    SegmentPropertiesInReduxHandler
+} from "./segments/segments-reducer";
 import { UsersBscsToSapMappings } from "./bscs-account/bscs-account-reducer";
-import { AccountMappingType } from "./bscs-account/bscs-account-store-type";
+import { tabsState } from "./tabReducer";
 
 export type MainStateType = {
     auth: AuthType,
@@ -27,7 +34,7 @@ export const mainReducer = combineReducers(
     // add reducers
     {
         auth: createReducer(new AuthType(), [UnauthorizedHandler(), PostUserLoginHandler(), PostUserInfoHandler(), PostUserLogoffHandler()]),
-        segments: createReducer({}, [GetDictionarySegmentHandler(), PostDictionarySegmentHandler(), SegmentPropertiesInReduxHandler()]),
+        segments: createReducer(new SegmentsType(), [GetDictionarySegmentHandler(), PostDictionarySegmentHandler(), SegmentPropertiesInReduxHandler()]),
         backendInfo: createReducer({ version: '?' }, [GetSystemVersionHandler()]),
         accountMapping: createReducer(new AccountMappingType(), [UsersBscsToSapMappings()]),
         sapAccountOfi: createReducer(new SapAccountStoreType(), [GetDictionaryAccountSapHandler(), PostDictionaryAccountSapHandler(), DeleteDictionaryAccountSapHandler()]),
